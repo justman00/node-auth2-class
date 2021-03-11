f2958312943ee01f628aa61efdb109b6ea169063
Vladimir Turcan <vladimir.turcan@sumup.com>
2021-03-11 20:16:12 +0200

    prepare exercise

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const helmet = require("helmet")
 const cors = require("cors")
-const session = require("express-session")
+const cookieParser = require('cookie-parser');
 const usersRouter = require("./users/users-router")
 
 const server = express()
@@ -10,11 +10,7 @@ const port = process.env.PORT || 5000
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
-server.use(session({
-	resave: false, // avoid recreating sessions that have not changed
-	saveUninitialized: false, // comply with GDPR laws for setting cookies automatically
-	secret: "keep it secret, keep it safe", // cryptographically sign the cookie
-}))
+server.use(cookieParser());
 
 server.use(usersRouter)
 server.use((err, req, res, next) => {
--- users/users-middleware.js
+++ users/users-middleware.js
@@ -1,21 +1,24 @@
-function restrict() {
-	return async (req, res, next) => {
-		try {
-			// express-session will automatically get the session ID from the cookie
-			// header, and check to make sure it's valid and the session for this user exists.
-			if (!req.session || !req.session.user) {
-				return res.status(401).json({
-					message: "Invalid credentials",
-				})
-			}
+const jwt = require('jsonwebtoken');
 
-			next()
-		} catch(err) {
-			next(err)
-		}
-	}
+function restrict() {
+  return async (req, res, next) => {
+    try {
+      const token = req.cookies.token;
+
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).json({ message: 'Invalid credentials!' });
+        }
+        req.decoded = decoded;
+
+        next();
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
 }
 
 module.exports = {
-	restrict,
-}
+  restrict,
+};
